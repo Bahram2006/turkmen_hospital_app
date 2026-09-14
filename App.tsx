1a546c846ab950f7ee09c965b64271773a0f0591
Bahram2006 <yardam034@gmail.com>
2026-09-14 12:41:03 +0500

    feat(app): wrap entry point with AppointmentProvider and NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
+import { AppointmentProvider } from './src/context/AppointmentContext';
 import RootNavigator from './src/navigation/RootNavigator';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <AppointmentProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AppointmentProvider>
     </AuthProvider>
   );
 };
